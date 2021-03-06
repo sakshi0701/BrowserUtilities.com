c54b9146156782551050144cc7a824193bd24326
Sakshi Dwivedi <sakshithedwivedi@gmail.com>
2021-03-07 02:54:08 +0530

    Adding navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import './App.css'
+import Navbar from './components/navbar/Navbar'
 import PasswordGen from './password generator/PasswordGen';
 import QRCode from './QR Code/QRCode';
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
+    <>
+    <Navbar />
     <div className="App">
       <PasswordGen />
       <QRCode />
     </div>
+    <Footer />
+    </>
   );
 }
 
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './nav.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            BrowserUtilities.com
+        </div>
+    )
+}
+
+export default Navbar
